31a254c55d9bdd5a3d8cdc8e550b2dc75b17662f
nguyenthang2469 <nguyenthangcbq@gmail.com>
2023-05-29 17:40:45 +0700

    Init MVC

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars').engine;
+const app = express();
+const port = 3000;
+
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({ extended: true })); // use body-parser in express version ^4.16 integrate to parse form data
+app.use(express.json()); // parse body from fetch, axios,...
+
+// Http logger
+app.use(morgan('combined'));
+
+// Template engine
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+// Routes init
+route(app);
+
+app.listen(port, () =>
+    console.log(`Example app listening at http://localhost:${port}`),
+);
